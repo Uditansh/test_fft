2730ac94f0fb38e9d28ff562d1bd8bbda1386b6c
Uditansh <uditanshb@gmail.com>
2023-11-30 18:26:54 +0530

    slide buttons

--- src/App.js
+++ src/App.js
@@ -1,81 +1,98 @@
-import React, { useState, useEffect } from 'react';
-import { fft, util as fftUtil } from 'fft-js';
-import Plot from 'react-plotly.js';
-import { ref, onValue } from './firebase'; // Adjust the path accordingly
-import { database } from './firebase'; 
+import React, { useState, useEffect, useRef } from "react";
+import { fft, util as fftUtil } from "fft-js";
+import Plot from "react-plotly.js";
+import { ref, onValue } from "./firebase"; // Adjust the path accordingly
+import { database } from "./firebase";
+
 function App() {
   const [fftData, setFFTData] = useState([]);
   const [keyValueData, setKeyValueData] = useState([]);
- 
-  
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const [windowStartIndex, setWindowStartIndex] = useState(0);
+  const [latestDataIndex, setLatestDataIndex] = useState(0);
+  const windowSize = 10; // Number of data points to show in the window
+  const dataRef = useRef(ref(database, "measures"));
+
   const calculateFFT = (data) => {
+    console.log(data);
     // Ensure data is not empty or undefined
     if (!data || Object.keys(data).length === 0) {
-      console.error('Invalid data for FFT calculation');
+      console.error("Invalid data for FFT calculation");
       return;
     }
-  
-    const values = Object.keys(data).sort((a, b) => Number(a) - Number(b)).map((key) => Number(data[key]));
-  
+
+    const fftResult = Object.keys(data).map((uniqueId) => {
+      const { time, value } = data[uniqueId];
+      return { time, value };
+    });
+
+    setKeyValueData(fftResult);
+
+    const values = fftResult.map((point) => point.value);
+
     // Ensure values array is not empty
     if (values.length === 0) {
-      console.error('Values array for FFT calculation is empty');
+      console.error("Values array for FFT calculation is empty");
       return;
     }
-  
+
     // Pad the values array to the next power of 2
     const paddedValues = padData(values);
-  
+
     // Perform FFT calculation
     const phasors = fft(paddedValues);
     const frequencies = fftUtil.fftFreq(phasors, 8000);
     const magnitudes = fftUtil.fftMag(phasors);
-  
-    const fftResult = frequencies.map((f, ix) => ({
+
+    const fftResultData = frequencies.map((f, ix) => ({
       frequency: f,
       magnitude: magnitudes[ix],
     }));
-  
-    setFFTData(fftResult);
+
+    setFFTData(fftResultData);
+
+    // Update the latest data index
+    setLatestDataIndex(fftResult.length - 1);
   };
-  
- 
-const padData = (data) => {
-  const originalLength = data.length;
-  const nextPowerOf2 = Math.pow(2, Math.ceil(Math.log2(originalLength)));
-  const paddedLength = Math.max(originalLength, nextPowerOf2); // Ensure padding for odd lengths
 
-  return data.concat(Array(paddedLength - originalLength).fill(0));
-};
+  const padData = (data) => {
+    const originalLength = data.length;
+    const nextPowerOf2 = Math.pow(2, Math.ceil(Math.log2(originalLength)));
+    const paddedLength = Math.max(originalLength, nextPowerOf2); // Ensure padding for odd lengths
 
-  
-  
-  
+    return data.concat(Array(paddedLength - originalLength).fill(0));
+  };
 
   useEffect(() => {
-    const dataRef = ref(database, 'eegData');
-  
-    onValue(dataRef, (snapshot) => {
+    const onDataChange = (snapshot) => {
       const data = snapshot.val();
-      console.log('Firebase Data:', data);
-  
+      console.log("Firebase Data:", data);
+
       if (data != null) {
         calculateFFT(data);
-  
-        const keyValueResult = Object.keys(data).map((key) => ({
-          key: Number(key),
-          value: Number(data[key]),
-        }));
-  
-        console.log('Key-Value Data:', keyValueResult);
-        setKeyValueData(keyValueResult);
+
+        // Set the initial windowStartIndex to display the latest data window
+        setWindowStartIndex(Math.max(0, latestDataIndex - windowSize + 1));
       }
-    });
-  }, [calculateFFT]);
-  
-  
+    };
+
+    const unsubscribe = onValue(dataRef.current, onDataChange);
+
+    return () => {
+      // Unsubscribe when the component is unmounted
+      unsubscribe();
+    };
+  }, [windowSize, latestDataIndex]);
+
+  const handleNextClick = () => {
+    setWindowStartIndex((prevIndex) => Math.min(prevIndex + windowSize, latestDataIndex - windowSize + 1));
+  };
+
+  const handlePrevClick = () => {
+    setWindowStartIndex((prevIndex) => Math.max(0, prevIndex - windowSize));
+  };
+
+  const slicedFFTData = fftData.slice(windowStartIndex, windowStartIndex + windowSize);
+  const slicedKeyValueData = keyValueData.slice(windowStartIndex, windowStartIndex + windowSize);
 
   return (
     <div className="App">
@@ -85,19 +102,19 @@ const padData = (data) => {
         <Plot
           data={[
             {
-              x: fftData.map((point) => point.frequency),
-              y: fftData.map((point) => point.magnitude),
-              type: 'scatter',
-              mode: 'lines',
-              name: 'FFT Plot',
+              x: slicedFFTData.map((point) => point.frequency),
+              y: slicedFFTData.map((point) => point.magnitude),
+              type: "scatter",
+              mode: "lines",
+              name: "FFT Plot",
             },
           ]}
           layout={{
             width: 800,
             height: 400,
-            title: 'FFT Plot',
-            xaxis: { title: 'Frequency (Hz)' },
-            yaxis: { title: 'Magnitude' },
+            title: "FFT Plot",
+            xaxis: { title: "Frequency (Hz)" },
+            yaxis: { title: "Magnitude" },
           }}
         />
       </div>
@@ -106,21 +123,29 @@ const padData = (data) => {
         <Plot
           data={[
             {
-              x: keyValueData.map((point) => point.key),
-              y: keyValueData.map((point) => point.value),
-              type: 'scatter',
-              mode: 'lines',
-              name: 'Key-Value Plot',
+              x: slicedKeyValueData.map((point) => point.time),
+              y: slicedKeyValueData.map((point) => point.value),
+              type: "scatter",
+              mode: "lines",
+              name: "Key-Value Plot",
             },
           ]}
           layout={{
             width: 800,
             height: 400,
-            title: 'Key-Value Plot',
-            xaxis: { title: 'Key' },
-            yaxis: { title: 'Value' },
+            title: "Key-Value Plot",
+            xaxis: { title: "Key" },
+            yaxis: { title: "Value" },
           }}
         />
+      </div>
+      <div>
+        <button onClick={handlePrevClick} disabled={windowStartIndex === 0}>
+          Previous
+        </button>
+        <button onClick={handleNextClick} disabled={windowStartIndex + windowSize >= latestDataIndex}>
+          Next
+        </button>
       </div>
     </div>
   );
