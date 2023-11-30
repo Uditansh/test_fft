bf6a07fcaf419237123e9539c21280ef4857b044
manyabansal <maanyab007@gmail.com>
2023-11-30 15:39:47 +0530

    graph plotted from firebase using potentiometer

--- src/App.js
+++ src/App.js
@@ -1,81 +1,95 @@
-import React, { useState, useEffect } from 'react';
-import { fft, util as fftUtil } from 'fft-js';
-import Plot from 'react-plotly.js';
-import { ref, onValue } from './firebase'; // Adjust the path accordingly
-import { database } from './firebase'; 
+import React, { useState, useEffect } from "react";
+import { fft, util as fftUtil } from "fft-js";
+import Plot from "react-plotly.js";
+import { ref, onValue } from "./firebase"; // Adjust the path accordingly
+import { database } from "./firebase";
 function App() {
   const [fftData, setFFTData] = useState([]);
   const [keyValueData, setKeyValueData] = useState([]);
- 
-  
-  
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+  useEffect(() => {
+    const dataRef = ref(database, "measures");
+
+    onValue(dataRef, (snapshot) => {
+      const data = snapshot.val();
+      console.log("Firebase Data:", data);
+
+      if (data != null) {
+        calculateFFT(data);
+      }
+    });
+  }, [fftData]);
+
   const calculateFFT = (data) => {
+    console.log(data);
     // Ensure data is not empty or undefined
     if (!data || Object.keys(data).length === 0) {
-      console.error('Invalid data for FFT calculation');
+      console.error("Invalid data for FFT calculation");
       return;
     }
-  
-    const values = Object.keys(data).sort((a, b) => Number(a) - Number(b)).map((key) => Number(data[key]));
-  
+
+    const fftResult = Object.keys(data).map((uniqueId) => {
+      const { time, value } = data[uniqueId];
+      return { time, value };
+    });
+
+    setKeyValueData(fftResult);
+
+    const values = fftResult.map((point) => point.value);
+
     // Ensure values array is not empty
     if (values.length === 0) {
-      console.error('Values array for FFT calculation is empty');
+      console.error("Values array for FFT calculation is empty");
       return;
     }
-  
+
     // Pad the values array to the next power of 2
     const paddedValues = padData(values);
-  
+
     // Perform FFT calculation
     const phasors = fft(paddedValues);
     const frequencies = fftUtil.fftFreq(phasors, 8000);
     const magnitudes = fftUtil.fftMag(phasors);
-  
-    const fftResult = frequencies.map((f, ix) => ({
+
+    const fftResultData = frequencies.map((f, ix) => ({
       frequency: f,
       magnitude: magnitudes[ix],
     }));
-  
-    setFFTData(fftResult);
+
+    setFFTData(fftResultData);
   };
-  
- 
-const padData = (data) => {
-  const originalLength = data.length;
-  const nextPowerOf2 = Math.pow(2, Math.ceil(Math.log2(originalLength)));
-  const paddedLength = Math.max(originalLength, nextPowerOf2); // Ensure padding for odd lengths
-
-  return data.concat(Array(paddedLength - originalLength).fill(0));
-};
-
-  
-  
-  
 
   useEffect(() => {
-    const dataRef = ref(database, 'eegData');
-  
+    const dataRef = ref(database, "measures");
+
     onValue(dataRef, (snapshot) => {
       const data = snapshot.val();
-      console.log('Firebase Data:', data);
-  
+      console.log("Firebase Data:", data);
+
       if (data != null) {
         calculateFFT(data);
-  
-        const keyValueResult = Object.keys(data).map((key) => ({
-          key: Number(key),
-          value: Number(data[key]),
-        }));
-  
-        console.log('Key-Value Data:', keyValueResult);
+
+        const keyValueResult = Object.keys(data).map((uniqueId) => {
+          const { time, value } = data[uniqueId];
+          return { time, value };
+        });
+
+        console.log("Key-Value Data:", keyValueResult);
         setKeyValueData(keyValueResult);
       }
     });
   }, [calculateFFT]);
-  
-  
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+
+
+
+  const padData = (data) => {
+    const originalLength = data.length;
+    const nextPowerOf2 = Math.pow(2, Math.ceil(Math.log2(originalLength)));
+    const paddedLength = Math.max(originalLength, nextPowerOf2); // Ensure padding for odd lengths
+
+    return data.concat(Array(paddedLength - originalLength).fill(0));
+  };
 
   return (
     <div className="App">
@@ -87,17 +101,17 @@ const padData = (data) => {
             {
               x: fftData.map((point) => point.frequency),
               y: fftData.map((point) => point.magnitude),
-              type: 'scatter',
-              mode: 'lines',
-              name: 'FFT Plot',
+              type: "scatter",
+              mode: "lines",
+              name: "FFT Plot",
             },
           ]}
           layout={{
             width: 800,
             height: 400,
-            title: 'FFT Plot',
-            xaxis: { title: 'Frequency (Hz)' },
-            yaxis: { title: 'Magnitude' },
+            title: "FFT Plot",
+            xaxis: { title: "Frequency (Hz)" },
+            yaxis: { title: "Magnitude" },
           }}
         />
       </div>
@@ -106,19 +120,19 @@ const padData = (data) => {
         <Plot
           data={[
             {
-              x: keyValueData.map((point) => point.key),
+              x: keyValueData.map((point) => point.time),
               y: keyValueData.map((point) => point.value),
-              type: 'scatter',
-              mode: 'lines',
-              name: 'Key-Value Plot',
+              type: "scatter",
+              mode: "lines",
+              name: "Key-Value Plot",
             },
           ]}
           layout={{
             width: 800,
             height: 400,
-            title: 'Key-Value Plot',
-            xaxis: { title: 'Key' },
-            yaxis: { title: 'Value' },
+            title: "Key-Value Plot",
+            xaxis: { title: "Key" },
+            yaxis: { title: "Value" },
           }}
         />
       </div>
